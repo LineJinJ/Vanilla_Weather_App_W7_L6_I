8d198a51181e6f90960b4a8891c9b5bfd21303f4
LineJinJ <102582136+LineJinJ@users.noreply.github.com>
2022-06-16 13:27:33 +0200

    Updated cityElement - New York

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.na  me;
 }
 
 let apiKey = "8469b2792b7396f5ad1a7f0ec5ad59c0";
