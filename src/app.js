40dad633a0034cced63195c2d6dc3cc764143ea6
LineJinJ <102582136+LineJinJ@users.noreply.github.com>
2022-06-16 13:22:47 +0200

    Updated temperature and rounded up to whole integer

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+}
+
 let apiKey = "8469b2792b7396f5ad1a7f0ec5ad59c0";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York
+&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
