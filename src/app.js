91265f0fa318aa61eceffd6f0be8f1a1e47419d0
LineJinJ <102582136+LineJinJ@users.noreply.github.com>
2022-06-16 13:33:35 +0200

    Updated description of weather in JS

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
-
+  let descriptionElement = document.querySelector("#description");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  cityElement.innerHTML = response.data.na  me;
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "8469b2792b7396f5ad1a7f0ec5ad59c0";
